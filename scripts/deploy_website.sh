9ed25c5eea584bd500e5c1f9585cbbe49c88a286
Rebecca Dittmar <9spire@gmail.com>
2021-04-18 22:30:37 -0700

    first scripts

--- scripts/deploy_website.sh
+++ scripts/deploy_website.sh
@@ -1,1 +1,2 @@
 echo "I am running: $0"
+docker-compose run --rm --entrypoint "aws s3 cp --recursive website/ s3://project.rdittmar.com" aws
--- scripts/unit_tests.sh
+++ scripts/unit_tests.sh
@@ -1,1 +1,3 @@
 echo "I am running: $0"
+docker-compose up -d website selenium
+docker-compose run --rm unit-tests
