fe5151463a81e2f998397ad4ba4a24e4eca6e19c
Rebecca Dittmar <9spire@gmail.com>
2021-04-20 13:59:53 -0700

    test over, commented out failing lines for use later

--- scripts/deploy_website.sh
+++ scripts/deploy_website.sh
@@ -1,2 +1,2 @@
 echo "I am running: $0"
-docker-compose run --rm --entrypoint "aws s3 cp --recursive website/ s3://project.rdittmar.com" aws
+# docker-compose run --rm --entrypoint "aws s3 cp --recursive website/ s3://project.rdittmar.com" aws
